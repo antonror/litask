b477eabbb01c78f9342342e3eb24178dd00e5ac3
antonror <hitty@i.ua>
2017-06-09 17:26:24 +0200

    Added SignUp path + user_new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#about'
   get  '/about',   to: 'static_pages#about'
-
+  get '/signup',   to: 'users#new'
   resources :tasks
   resources :users
 
