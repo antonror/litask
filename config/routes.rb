f6f37061e17e99a98f5bb82c6ca29c5ae19b8784
antonror <hitty@i.ua>
2017-06-11 15:56:38 +0200

    ADDED: Tasks CRUD Action; Task access control

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root 'static_pages#about'
-  get  '/about',   to: 'static_pages#about'
-  get '/signup',   to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  resources :tasks
+  get  '/about',      to: 'static_pages#about'
+  get '/signup',      to: 'users#new'
+  get    '/login',    to: 'sessions#new'
+  post   '/login',    to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy'
+  resources :tasks,   only: [:create, :destoy]
   resources :users
 
 end
